c350ae8d90654611e2ed4f8a27dca4086de97b8c
siddontang <siddontang@gmail.com>
2015-02-27 21:01:50 +0800

    fix flag overwrite config file
    for https://github.com/siddontang/redis-failover/issues/3

--- main.go
+++ main.go
@@ -11,23 +11,23 @@ import (
 )
 
 var configFile = flag.String("config", "", "failover config file")
-var addr = flag.String("addr", ":11000", "failover http listen addr")
-var checkInterval = flag.Int("check_interval", 1000, "check master alive every n millisecond")
-var maxDownTime = flag.Int("max_down_time", 3, "max down time for a master, after that, we will do failover")
+var addr = flag.String("addr", "", "failover http listen addr")
+var checkInterval = flag.Int("check_interval", 0, "check master alive every n millisecond")
+var maxDownTime = flag.Int("max_down_time", 0, "max down time for a master, after that, we will do failover")
 
 var masters = flag.String("masters", "", "redis master need to be monitored, seperated by comma")
 var mastersState = flag.String("masters_state", "", "new or existing for raft, if new, we will depracted old saved masters")
 
 var broker = flag.String("broker", "", "broker for cluster, now is raft or zk")
 
-var raftDataDir = flag.String("raft_data_dir", "./var/store", "raft data store path")
-var raftLogDir = flag.String("raft_log_dir", "./var/log", "raft log store path")
+var raftDataDir = flag.String("raft_data_dir", "", "raft data store path")
+var raftLogDir = flag.String("raft_log_dir", "", "raft log store path")
 var raftAddr = flag.String("raft_addr", "", "raft listen addr, if empty, we will disable raft")
 var raftCluster = flag.String("raft_cluster", "", "raft cluster,vseperated by comma")
 var raftClusterState = flag.String("raft_cluster_state", "", "new or existing, if new, we will deprecate old saved cluster and use new")
 
 var zkAddr = flag.String("zk_addr", "", "zookeeper address, seperated by comma")
-var zkPath = flag.String("zk_path", "/zk/redis/failover", "base directory in zk, prefix must be /zk")
+var zkPath = flag.String("zk_path", "", "base directory in zk, prefix must be /zk")
 
 func main() {
 	flag.Parse()
@@ -44,6 +44,12 @@ func main() {
 		fmt.Printf("no config file, use default config")
 		c = new(failover.Config)
 		c.Addr = ":11000"
+		c.CheckInterval = 1000
+		c.MaxDownTime = 3
+	}
+
+	if len(*addr) > 0 {
+		c.Addr = *addr
 	}
 
 	if *checkInterval > 0 {
@@ -86,11 +92,6 @@ func main() {
 
 	if len(*broker) > 0 {
 		c.Broker = *broker
-	} else if len(c.Raft.Addr) > 0 {
-		// use raft
-		c.Broker = "raft"
-	} else if len(c.Zk.Addr) > 0 {
-		c.Broker = "zk"
 	}
 
 	seps = strings.Split(*masters, ",")
